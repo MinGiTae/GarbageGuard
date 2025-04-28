00a949ed758db04f8ed17ff7e8c1feec668ecf9d
Minani99 <minhwanpark@naver.com>
2025-04-28 13:00:58 +0900

    2025-04-28 백업 및 recycle 임시 프론트 구성

--- app.py
+++ app.py
@@ -4,15 +4,15 @@ from routes.stats_routes import stats_bp
 from routes.gallery_routes import gallery_bp
 from routes.predict_routes import predict_bp
 from routes.csr_routes import csr_bp
-from routes.createlift_routes import createlift_bp  # 🔹 Create Lift 라우트 추가
-# from routes.upload_routes import upload_bp
+from routes.createlift_routes import createlift_bp
+from routes.upload_routes import upload_bp
 
 app = Flask(__name__)
 app.secret_key = 'go_home'
 
-# ✅ Blueprint 등록 (URL prefix도 함께)
+# Blueprint 등록 (URL prefix도 함께)
 app.register_blueprint(main_bp)                            # /
-# app.register_blueprint(upload_bp, url_prefix='/upload')    # /upload/*
+app.register_blueprint(upload_bp, url_prefix='/upload')    # /upload/*
 app.register_blueprint(stats_bp)                           # /region-data/*
 app.register_blueprint(gallery_bp)                         # /gallery
 app.register_blueprint(predict_bp)                         # /input_waste
@@ -21,5 +21,6 @@ app.register_blueprint(createlift_bp)                      # /Create_lift  ← �
 
 
 if __name__ == '__main__':
+    import pprint
+    pprint.pprint(app.url_map.iter_rules())
     app.run(debug=True)
-
